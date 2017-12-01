80e8d943a1a8ace86da269b416eb1abe7204eaed
mehmet <memojja1907@gmail.com>
2017-12-01 14:05:18 +0300

    added async methods

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,56 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @EnableAsync
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+
+	private final GitHubLookupService gitHubLookupService;
+
+	@Autowired
+	public DemoApplication(GitHubLookupService gitHubLookupService) {
+		this.gitHubLookupService = gitHubLookupService;
+	}
+
+	//for customize
+	@Bean
+	public Executor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(3);
+		executor.setMaxPoolSize(3);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("memojja-");
+		executor.initialize();
+		return executor;
+	}
+
+
+	@Override
+	public void run(String... strings) throws Exception {
+
+		// Kick of multiple, asynchronous lookups
+		CompletableFuture<User> page0 = gitHubLookupService.findUser("memojja");
+		CompletableFuture<User> page1 = gitHubLookupService.findUser("PivotalSoftware");
+		CompletableFuture<User> page2 = gitHubLookupService.findUser("CloudFoundry");
+		CompletableFuture<User> page3 = gitHubLookupService.findUser("Spring-Projects");
+
+		// Wait until they are all done
+		CompletableFuture.allOf(page1,page2,page3).join();
+
 	}
 }
---
+++ src/main/java/com/example/demo/GitHubLookupService.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class GitHubLookupService {
+    private static final Logger logger = LoggerFactory.getLogger(GitHubLookupService.class);
+
+    private final RestTemplate restTemplate;
+
+    public GitHubLookupService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Async
+    public CompletableFuture<User> findUser(String user) throws InterruptedException {
+        logger.info("Looking up " + user);
+        System.out.println("CompletableFuture method");
+
+        String url = String.format("https://api.github.com/users/%s", user);
+        User results = restTemplate.getForObject(url, User.class);
+        // Artificial delay of 1s for demonstration purposes
+        Thread.sleep(5000L);
+        return CompletableFuture.completedFuture(results);
+    }
+
+}
